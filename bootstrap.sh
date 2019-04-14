3ad106f1867fb3631a0972e11b6362f482e7a6b6
nestoralonsovina <alonsovinanestor@gmail.com>
2019-04-14 23:50:41 +0200

    things

---
+++ bootstrap.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+brew () {
+    which brew 1>&/dev/null
+    if [ ! "$?" -eq 0 ] ; then
+        echo "Homebrew not installed. Attempting to install Homebrew"
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        if [ ! "$?" -eq 0 ] ; then
+            echo "Something went wrong. Exiting..." && exit 1
+        fi
+    fi
+    echo "Installing last version of Vim\n"
+    brew install vim
+
+    echo "Installing last version of Python\n"
+    brew install python
+
+    echo "Installing gdb\n"
+    brew install gdb
+
+    brew tap caskroom/fonts
+    brew cask install font-fira-code
+
+    brew cask install hyper
+
+    brew cleanup
+}
+
+init () {
+    echo "Making a Projects folder in $PATH_TO_PROJECTS if it doesn't already exist"
+    mkdir -p "$PATH_TO_PROJECTS"
+    echo "Making a Playground folder in $PATH_TO_PLAYGROUND if it doesn't already exist"
+    mkdir -p "$PATH_TO_PLAYGROUND"
+}
+
+link () {
+    echo "This utility will symlink the files in this repo to the home directory"
+    echo "Proceed? (y/n)"
+    read resp
+    if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
+        for file in $( ls -A | grep -vE '\.exclude*|\.git$|\.gitignore|.*.md' ) ; do
+            ln -sv "$PWD/$file" "$HOME"
+        done
+        echo "Symlinking complete"
+    else
+        echo "Symlinking cancelled by user"
+        return 1
+    fi
+}
+
+install_tools () {
+    if [ $( echo "$OSTYPE" | grep 'darwin' ) ] ; then
+        echo "This utility will install useful utilities using Homebrew"
+        echo "Proceed? (y/n)"
+        read resp
+        if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
+            echo "Installing useful stuff using brew. This may take a while..."
+            sh brew.exclude.sh
+        else
+            echo "Brew installation cancelled by user"
+        fi
+    else
+        echo "Skipping installations using Homebrew because MacOS was not detected..."
+    fi
+}
+
+init
+link
+install_tools
